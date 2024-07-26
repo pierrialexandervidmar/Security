a213ee746b7d97da47c25b4c9e8e03d6443490ba
pierrialexandervidmar <pierri@hotmail.com.br>
2024-07-25 21:15:35 -0300

    Criada camadas

---
+++ src/main/java/com/springsecurity/security/service/UserService.java
@@ -0,0 +1,12 @@
+package com.springsecurity.security.service;
+
+import com.springsecurity.security.entity.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    public User create(User user) {
+        return null;
+    }
+}
